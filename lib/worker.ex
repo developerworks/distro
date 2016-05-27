a3e215d3cda935f2ddbb05a089decf2b3bd5530e
developerworks <developerworks@163.com>
2016-05-27 16:55:00 +0800

    Add load check

--- lib/worker.ex
+++ lib/worker.ex
@@ -3,6 +3,8 @@ defmodule Distro.Worker do
 	use GenServer
   require Logger
 
+  @on_load :load_check
+
   def start_link do
     GenServer.start_link(__MODULE__, [], name: {:global, __MODULE__})
   end
@@ -14,5 +16,9 @@ defmodule Distro.Worker do
   def handle_info(:timeout, state) do
     Logger.debug "timeout"
     {:noreply, state, 1000}
+  end
+
+  def load_check do
+    Logger.debug "Module #{__MODULE__} is loaded."
   end
 end
